0c0b56a907c33f13c020a5f98a042a54d2672828
Miikka Alatalo <miikka.alatalo@gmail.com>
2017-11-14 22:12:18 +0200

    add stuff

--- RadBot.js
+++ RadBot.js
@@ -1,22 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const conf = require("./conf.json");
+
+const soundboardPrivilege = conf.soundboardPrivilege;
+const prefix = conf.prefix;
 
 client.on('ready', () => {
-    
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log("Logged in as "+client.user.tag+"!");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if(message.member.roles.find("name", conf.soundboardPrivilege)){
+    console.log("jukka");
+  }
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
   }
 });
 
-const getBotClientToken = () => {
-    const fs = require("fs");
-    const confjson = fs.readFileSync("conf.json");
-    const conf = JSON.parse(confjson);
-    return conf.botClientToken;
-}
-
-client.login(getBotClientToken());
+client.login(conf.token);
---
+++ readme.md
@@ -0,0 +1,4 @@
+Discord bot using Discord.js  
+
+https://discord.js.org  
+https://anidiotsguide.gitbooks.io/discord-js-bot-guide/
